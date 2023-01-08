b8d66edb92b13eb5aeea959cb95e7cf5c5087676
MohamedAshraf2 <Hamoudy495@gmail.com>
2023-01-08 17:46:05 +0200

    init 2 app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,10 +9,12 @@ export const environment = {
   firebaseConfig : {
     apiKey: "AIzaSyDxtLeqwR3AMlQYM6zk7Uz4e8_Dk0MZWoI",
     authDomain: "clinido-ce15d.firebaseapp.com",
+    databaseURL: "https://clinido-ce15d-default-rtdb.firebaseio.com",
     projectId: "clinido-ce15d",
     storageBucket: "clinido-ce15d.appspot.com",
     messagingSenderId: "584753027934",
-    appId: "1:584753027934:web:719e525a3580d8fbad8dcf"
+    appId: "1:584753027934:web:719e525a3580d8fbad8dcf",
+    measurementId: "G-JNX1CVNNNJ"
   }
 };
 
